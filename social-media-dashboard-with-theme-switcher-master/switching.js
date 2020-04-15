65af8880328a132c51aac95d4da705395eabdab6
fadyjan <fady.jan95@gmail.com>
2020-04-15 13:07:00 +0200

    switch button between themes

---
+++ social-media-dashboard-with-theme-switcher-master/switching.js
@@ -0,0 +1,51 @@
+var x = 0
+var body = document.getElementsByTagName("body")
+var cards = document.getElementsByClassName("jscontrol")
+var gaps = document.getElementsByClassName("gaps")
+var fonts =document.getElementsByClassName("fontchange")
+function switching() {
+    if (x == 0) {    //switch into light mode
+
+
+        gaps[0].style.backgroundColor = "white"
+        body[0].style.backgroundColor = "white"
+
+        for (let index = 0; index < cards.length; index++) {
+            cards[index].style.backgroundColor = "hsl(227, 47%, 96%)";
+
+        }
+
+        for (let index = 0; index < fonts.length; index++) {
+            fonts[index].style.color=" hsl(230, 17%, 14%)"
+            
+        }
+
+
+
+
+
+
+        x = x + 1
+
+
+    }
+
+    else {
+
+        gaps[0].style.backgroundColor = "hsl(230, 17%, 14%)"
+        body[0].style.backgroundColor = "hsl(230, 17%, 14%)"
+
+        for (let index = 0; index < cards.length; index++) {
+            cards[index].style.backgroundColor = "hsl(228, 28%, 20%)";
+        }
+
+        for (let index = 0; index < fonts.length; index++) {
+            fonts[index].style.color=" hsl(0, 0%, 100%)"
+            
+        }
+        x = x - 1
+
+    }
+
+
+}
